bd5e5cc24d76a04315d092607813a6f4cafe0513
Oliver Atkinson <oatkinson@ehps.k12.mt.us>
2024-05-20 14:42:40 -0600

    from template to working

--- src/main.rs
+++ src/main.rs
@@ -5,91 +5,114 @@
 #![feature(addr_parse_ascii)]
 #![feature(async_closure)]
 
-use std::future::IntoFuture;
-
-use account::simple_storage::FilesystemDB;
-use clap::Parser;
+use anyhow::anyhow;
+use chrono::Local;
 use poem::{
-    EndpointExt, endpoint::StaticFilesEndpoint, listener::TcpListener, Route, Server
+    get, handler,
+    listener::TcpListener,
+    web::{Path, RealIp},
+    Request, Route, Server,
 };
-use tracing::{Level, instrument};
+use std::{env, future::IntoFuture};
+use tracing::{instrument, Level};
 use tracing_subscriber::EnvFilter;
-
-use crate::account::AppState;
-
-mod account;
-
-#[derive(Debug, Parser)]
-#[command(version)]
-struct Args {
-    #[arg(short, long)]
-    new: Option<String>,
-}
+use uuid::Uuid;
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-
+async fn main() -> Result<(), anyhow::Error> {
     #[cfg(debug_assertions)]
-    let filter = EnvFilter::builder()
-            .parse("room_clocks=trace,poem=debug,tokio=warn")
-            .expect("Could not create env filter.")
-            ;
+    {
+        let filter = EnvFilter::builder()
+            .parse("room_clocks=info,poem=info,tokio=warn")
+            .expect("Could not create env filter.");
 
-    #[cfg(debug_assertions)]
-    tracing_subscriber::fmt::fmt()
-        .with_max_level(Level::TRACE)
-        .with_target(true)
-        .with_env_filter(filter)
-        .with_thread_ids(false)
-        .with_file(false)
-        .without_time()
-        .init();
+        tracing_subscriber::fmt::fmt()
+            .with_max_level(Level::TRACE)
+            .with_target(true)
+            .with_env_filter(filter)
+            .with_thread_ids(false)
+            .with_file(false)
+            .without_time()
+            .init();
+    }
+    #[cfg(not(debug_assertions))]
+    {
+        let filter = EnvFilter::builder()
+            .parse("room_clocks=info,poem=warn,tokio=warn")
+            .expect("Could not create env filter.");
 
-    let args = Args::parse();
-
-    match args.new {
-        Some(a) => {
-            let mut x = a.split_ascii_whitespace();
-            let username = x.next().expect("You passed an empty string as credentials...");
-            let password = x.next().expect("You need to pass a password along with that username.");
-            // let acct = Account::new(&AccountLogin::new(password, username)).expect("Internal error whilst building account. Most likely during the password hashing process.");
-            if let Ok(act) = account::Account::try_new(username, password) {
-                println!("{}", act); 
-            }
-            return Ok(())
-        },
-        None => {/*continue*/},
+        tracing_subscriber::fmt::fmt()
+            .with_max_level(Level::TRACE)
+            .with_target(true)
+            .with_env_filter(filter)
+            .with_thread_ids(false)
+            .with_file(false)
+            .without_time()
+            .init();
     }
 
-    // Tick thru messages
-    run_server().into_future().await
+    parse().await
 }
 
-
-pub static FILESYSTEM_ROOT: &str = "./www/";
 #[instrument(level="trace", name="web" skip_all)]
 async fn run_server() -> Result<(), std::io::Error> {
-    let appstate = AppState::<FilesystemDB>::new().await;
 
-    // Wow, poem routing gets a bit messy
     let app = Route::new()
-        .nest("/", StaticFilesEndpoint::new(FILESYSTEM_ROOT)
-            .index_file("index.html")
-            .fallback_to_index()
-        )
-
-        .nest("/protected", Route::new()
-            .nest("/", StaticFilesEndpoint::new(FILESYSTEM_ROOT.to_owned()+"protected/")
-                .index_file("account.html") 
-                .fallback_to_index()
-            )
-            .with(appstate)
-        )
-        .with(poem::middleware::Tracing)
-    ;
-
+        .at("/:id", get(gotcha))
+        ;
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(app)
         .await
 }
 
+#[handler]
+fn gotcha(Path(path): Path<String>, ip: RealIp, req: &Request) {
+
+
+    let time = Local::now(); 
+    let ip = ip.0.map(|f| f.to_string()).unwrap_or("Invalid IP".to_string());
+    let headers = req.headers();
+    // let method = req.method().to_string();
+    // let ver = req.version();
+    let uri = req.uri();
+ 
+    eprintln!("{time}|{path}|{ip}|{uri}|{:?}", headers)
+}
+
+async fn parse() -> Result<(), anyhow::Error> {
+    let args = env::args().enumerate().collect::<Vec<(usize, String)>>();
+
+    for (index, word) in &args {
+        if word.starts_with("--") {
+            // read word arg
+            match word.split_at(2).1 {
+                "serve" => run_server().into_future().await?,
+                "generate" => {
+                    // args[index + 1].
+                    if let Some((_, name)) = &args.get(index + 1) {
+                        let id = Uuid::new_v4();
+                        println!("{id},{name}");
+                        return Ok(());
+                    } else {
+                        return Err(anyhow!("Must provide a name with --generate"));
+                    }
+                }
+                _ => (),
+            }
+        } else if word.starts_with("-") {
+            // read multi single args
+        } else {
+            // read word
+        }
+    }
+
+    Err(anyhow!(HELPTEXT))
+}
+
+const HELPTEXT: &str = r#"
+There is no default behavior.
+
+--generate NAME     Generate a new id/location pair and write it to standard out in CSV format.
+                    (Strings with spaces must be wrapped in quotes.)
+--serve             Start the server
+"#;
