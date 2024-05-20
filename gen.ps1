cd0bae35998c6cc0e7be118c70e99dddafea277f
Oliver Atkinson <oatkinson@ehps.k12.mt.us>
2024-05-20 16:30:41 -0600

    fix shortcut

--- gen.ps1
+++ gen.ps1
@@ -1,16 +1,13 @@
 # Get drive letter
 $DRIVE=Read-Host -Prompt "USB Drive Path. ex: 'D:\'"
 $IP=Read-Host -Prompt "Location that the server is at"
+$LOC=Read-Host -Prompt "Copy output of ./phisher --generate 'loc' here"
 
-$GEN=.\phisher --generate "Example Location"
-# $GEN="id,loc" # Testing line
-Write-Output $GEN >> gen-location.txt
-
-$GEN=$GEN -split ","
+$GEN=$LOC -split ","
 $ID=$GEN[0]
 
 # Create hidden folder and it's script
-$SCRIPT_LOC="$DRIVE\hidden\run.bat"
+$SCRIPT_LOC="$DRIVE\hidden\run.ps1"
 New-Item -ItemType Directory -Force -Path "$DRIVE\hidden"
 Write-Output "curl http://$IP/$ID" > $SCRIPT_LOC
 attrib +r +h +s "$DRIVE\hidden"
@@ -18,6 +15,9 @@ attrib +r +h +s "$DRIVE\hidden"
 # Create shortcut from script to root
 $ws = New-Object -ComObject WScript.Shell;
 $s = $ws.CreateShortcut("$DRIVE\test.lnk");
-$S.RelativePath = ".\hidden\run.bat"
+$S.TargetPath = "powershell.exe"
+$S.Arguments = "-ExecutionPolicy Bypass -File $SCRIPT_LOC"
+# for whatever reason 7 means minimized
+$S.WindowStyle = 7
 $S.IconLocation = "%SystemRoot%\System32\SHELL32.dll,3"
 $S.Save();
